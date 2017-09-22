eb3367a5fe766559df1e433290d023589fc8e8e3
cj.ohara <cj.ohara@firespring.com>
2017-09-21 22:27:17 -0500

    [GD-241] [GD-369] Made seed script interactive, added random chunk size seeding for donations and nonprofit slides.

--- packages/lambda/bin/seed.js
+++ packages/lambda/bin/seed.js
@@ -15,88 +15,188 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-const commander = require('commander');
-const Generator = require('../test/helpers/test').generate;
+const dotenv = require('dotenv');
+dotenv.config({path: `${__dirname}/../../../.env`});
+
+const _ = require('lodash');
+const fuzzy = require('fuzzy');
 const DonorsRepository = require('../src/repositories/donors');
+const Generator = require('../test/helpers/test').generate;
+const inquirer = require('inquirer');
+const inquirerAutocomplete = require('inquirer-autocomplete-prompt');
 const MessagesRepository = require('../src/repositories/messages');
 const NonprofitsRepository = require('../src/repositories/nonprofits');
 const NonprofitDonationsRepository = require('../src/repositories/nonprofitDonations');
 const NonprofitSlidesRepository = require('../src/repositories/nonprofitSlides');
 const PaymentTransactionsRepository = require('../src/repositories/paymentTransactions');
-const ReportsRepository = require('../src/repositories/reports');
-const UsersRepository = require('../src/repositories/users');
 
-const types = {
-	donation: {
-		data: [],
-		repository: new NonprofitDonationsRepository()
-	},
-	donor: {
-		data: [],
-		repository: new DonorsRepository()
-	},
-	message: {
-		data: [],
-		repository: new MessagesRepository()
-	},
-	nonprofit: {
-		data: [],
-		repository: new NonprofitsRepository()
-	},
-	paymentTransaction: {
-		data: [],
-		repository: new PaymentTransactionsRepository()
-	},
-	report: {
-		data: [],
-		repository: new ReportsRepository()
-	},
-	slide: {
-		data: [],
-		repository: new NonprofitSlidesRepository()
-	},
-	user: {
-		data: [],
-		repository: new UsersRepository()
-	}
-};
+/**
+ * Seed Donations
+ *
+ * @return {Promise}
+ */
+const seedDonations = function () {
+	const donorsRepository = new DonorsRepository();
+	const donationsRepository = new NonprofitDonationsRepository();
+	const nonprofitsRepository = new NonprofitsRepository();
+	const paymentTransactionsRepository = new PaymentTransactionsRepository();
 
-const seed = function () {
-	Object.keys(types).forEach(function (type) {
-		if (types[type].data.length) {
-			types[type].repository.batchUpdate(types[type].data).then(function () {
-				console.log(`seeded ${type}s`);
-			}).catch(function (err) {
-				console.log(err);
-			});
+	return nonprofitsRepository.getAll().then(function (results) {
+		if (!results || results.length === 0) {
+			return Promise.reject(new Error('No nonprofits found in stack: ' + process.env.AWS_STACK_NAME));
 		}
+		const options = _.map(results, function (nonprofit) {
+			return {name: nonprofit.legalName, value: nonprofit.uuid}
+		});
+		return inquirer.prompt([
+			{
+				type: 'list',
+				message: 'Select a nonprofit:',
+				name: 'nonprofitUuid',
+				choices: options
+			},
+			{
+				type: 'input',
+				message: 'How many donations would you like to seed:',
+				name: 'count',
+				default: '10'
+			}
+		]);
+	}).then(function (answers) {
+		const count = parseInt(answers.count);
+		const chunkSize = Math.floor(Math.random() * count) + 1;
+
+		const donations = _.chunk(Generator.modelCollection('donation', count), chunkSize);
+		const donors = Generator.modelCollection('donor', donations.length);
+		const paymentTransactions = Generator.modelCollection('paymentTransaction', donations.length);
+
+		return donorsRepository.batchUpdate(donors).then(function () {
+			paymentTransactionsRepository.batchUpdate(paymentTransactions);
+		}).then(function () {
+			let promise = Promise.resolve();
+			_.each(donations, function (chunk, i) {
+				_.each(chunk, function (donation) {
+					donation.donorUuid = donors[i].uuid;
+					donation.paymentTransactionUuid = paymentTransactions[i].uuid;
+					donation.nonprofitUuid = answers.nonprofitUuid;
+				});
+
+				promise = promise.then(function () {
+					return donationsRepository.batchUpdate(chunk);
+				});
+			});
+			return promise;
+		});
+	}).then(function () {
+		console.log('seeded donations');
 	});
 };
 
-const generate = function (count, relatedCount) {
-	count = count > 0 ? count : 10;
-	relatedCount = relatedCount > 0 ? relatedCount : 10;
+/**
+ * Seed Messages
+ *
+ * @return {Promise}
+ */
+const seedMessages = function () {
+	const messagesRepository = new MessagesRepository();
 
-	types.donor.data = Generator.modelCollection('donor', count);
-	types.message.data = Generator.modelCollection('message', count);
-	types.nonprofit.data = Generator.modelCollection('nonprofit', count);
-	types.paymentTransaction.data = Generator.modelCollection('paymentTransaction', count);
-	types.report.data = Generator.modelCollection('report', count);
-	types.user.data = Generator.modelCollection('user', count);
-	for (let i = 0; i < count; i++) {
-		Generator.modelCollection('donation', relatedCount, {donorUuid: types.donor.data[i].uuid, nonprofitUuid: types.nonprofit.data[i].uuid, paymentTransactionUuid: types.paymentTransaction.data[i].uuid}).forEach(function (donation) {
-			types.donation.data.push(donation);
-		});
-		Generator.modelCollection('slide', relatedCount, {nonprofitUuid: types.nonprofit.data[i].uuid}).forEach(function (slide) {
-			types.slide.data.push(slide);
-		});
-	}
+	return inquirer.prompt([
+		{
+			type: 'input',
+			message: 'How many nonprofits would you like to seed:',
+			name: 'count',
+			default: '10'
+		}
+	]).then(function (answers) {
+		const count = parseInt(answers.count);
+		const messages = Generator.modelCollection('message', count);
+		return messagesRepository.batchUpdate(messages);
+	}).then(function () {
+		console.log('seeded messages');
+	});
 };
 
-commander
-	.option('-c, --count <count>', 'Number of models to create', 'count')
-	.option('-r, --relatedCount <relatedCount>', 'Number of related models to create', 'relatedCount')
-	.parse(process.argv);
+/**
+ * Seed Nonprofits
+ *
+ * @return {Promise}
+ */
+const seedNonprofits = function () {
+	const nonprofitsRepository = new NonprofitsRepository();
+	const nonprofitSlidesRepository = new NonprofitSlidesRepository();
 
-generate(commander.count, commander.relatedCount);
-seed();
+	return inquirer.prompt([
+		{
+			type: 'input',
+			message: 'How many nonprofits would you like to seed:',
+			name: 'count',
+			default: '10'
+		}
+	]).then(function (answers) {
+		const count = parseInt(answers.count);
+		const nonprofits = Generator.modelCollection('nonprofit', count);
+
+		let promise = nonprofitsRepository.batchUpdate(nonprofits);
+		_.each(nonprofits, function (nonprofit) {
+			const slideCount = Math.floor(Math.random() * 8) + 1;
+			const slides = Generator.modelCollection('slide', slideCount, { nonprofitUuid: nonprofit.uuid });
+			_.each(slides, function (slide, i) {
+				slide.sortOrder = i;
+			});
+			promise = promise.then(function () {
+				return nonprofitSlidesRepository.batchUpdate(slides);
+			});
+		});
+		return promise;
+	}).then(function () {
+		console.log('seeded nonprofits');
+	});
+};
+
+/**
+ * Filter through available seeders
+ *
+ * @param answers
+ * @param input
+ * @return {Promise}
+ */
+const seedersSource = function (answers, input) {
+	input = input || '';
+	return new Promise(function (resolve) {
+		const seeders = [
+			'donations',
+			'messages',
+			'nonprofits'
+		];
+		const results = fuzzy.filter(input, seeders);
+		resolve(results.map(function (el) {
+			return el.original;
+		}));
+	});
+};
+
+/**
+ * Run the script
+ */
+inquirer.registerPrompt('autocomplete', inquirerAutocomplete);
+inquirer.prompt([
+	{
+		type: 'autocomplete',
+		message: 'Select a seeder to run:',
+		name: 'seeder',
+		source: seedersSource
+	}
+]).then(function (answer) {
+	switch (answer.seeder) {
+		case 'donations':
+			return seedDonations();
+		case 'messages':
+			return seedMessages();
+		case 'nonprofits':
+			return seedNonprofits();
+		default:
+			return Promise.reject(new Error('Invalid seeder: ' + answer.seeder));
+	}
+}).catch(function (err) {
+	console.log(err);
+});
