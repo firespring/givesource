3513d9f23100f2a894cdc17ac74c5e17c72e96e0
cj.ohara <cj.ohara@firespring.com>
2017-09-26 18:29:27 -0500

    [GD-258] [GD-402] Added Ckeditor form component

---
+++ packages/frontend/src/admin-pages/components/forms/Ckeditor.vue
@@ -0,0 +1,73 @@
+<template>
+    <vue-ckeditor v-if="loaded" v-model="localValue" :toolbar="toolbar" :id="id" :language="language" :extraplugins="plugins" :class="{ 'has-error': hasErrors }" :height="height">
+    </vue-ckeditor>
+    <div v-else style="height: 12rem; justify-content: center; align-items: center; display: flex;">
+        <div class="c-progress c-progress--spinner c-spinner-active">
+            <div class="c-spinner-layer c-spinner-orange-only">
+                <div class="c-spinner-circle-clipper left">
+                    <div class="c-spinner-circle"></div>
+                </div><div class="gap-patch">
+                <div class="c-spinner-circle"></div>
+            </div><div class="c-spinner-circle-clipper right">
+                <div class="c-spinner-circle"></div>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    module.exports = {
+    	data: function () {
+    		return {
+			    language: 'en-us',
+			    toolbar: [
+				    {name: 'styles', items: ['Format']},
+				    {name: 'basicstyles', items: ['Bold', 'Italic', 'Strike', '-', 'RemoveFormat']},
+				    {name: 'paragraph', items: ['NumberedList', 'BulletedList', '-', 'Blockquote']},
+				    {name: 'colors', items: ['TextColor', 'BGColor']},
+				    {name: 'links', items: ['Link', 'Unlink']},
+				    {name: 'tools', items: ['Maximize']}
+			    ],
+			    plugins: 'colorbutton,colordialog',
+
+                localValue: '',
+            }
+        },
+        watch: {
+    		localValue: function (value, oldValue) {
+    			const vue = this;
+    			if (value === oldValue) {
+    				return;
+                }
+                vue.$emit('input', value);
+            },
+	        value: function (value, oldValue) {
+		        const vue = this;
+		        if (value === oldValue) {
+			        return;
+		        }
+		        vue.localValue = value;
+	        }
+        },
+    	props: {
+    		value: {},
+    		id: {
+    			type: String,
+                default: 'editor'
+            },
+		    loaded: {
+			    type: Boolean,
+			    default: false
+		    },
+            hasErrors: {
+    			type: Boolean,
+                default: false
+            },
+            height: {
+    			type: String,
+                default: '200'
+            }
+        }
+    };
+</script>
