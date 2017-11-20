9df4f5f400bc355f0e1f976e9a4ecf8629623590
cj.ohara <cj.ohara@firespring.com>
2017-11-20 15:31:42 -0600

    [GD-565] [GD-599] Added Sponsor and Sponsor Tiers models and repositories

---
+++ packages/lambda/src/models/sponsor.js
@@ -0,0 +1,78 @@
+/*
+ * Copyright (C) 2017  Firespring
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const Model = require('./model');
+
+/**
+ * Sponsor constructor
+ *
+ * @param {{}} [data]
+ * @constructor
+ */
+function Sponsor(data) {
+	Model.call(this, data);
+}
+
+/**
+ * Extend the base Model
+ *
+ *@type {model}
+ */
+Sponsor.prototype = new Model();
+
+/**
+ * The allowed attributes for this model
+ *
+ * @type {[*]}
+ */
+Sponsor.prototype.attributes = [
+	'fileUuid',
+	'logoUrl',
+	'name',
+	'sponsorTierUuid',
+	'url',
+];
+
+/**
+ * Validation constraints for this model
+ *
+ * @type {{}}
+ */
+Sponsor.prototype.constraints = {
+	fileUuid: {
+		presence: false,
+		uuid: 4
+	},
+	logoUrl: {
+		presence: false,
+		type: 'string'
+	},
+	name: {
+		presence: true,
+		type: 'string'
+	},
+	sponsorTierUuid: {
+		presence: true,
+		uuid: 4
+	},
+	url: {
+		presence: false,
+		url: true,
+	},
+};
+
+module.exports = Sponsor;
---
+++ packages/lambda/src/repositories/sponsors.js
@@ -0,0 +1,176 @@
+/*
+ * Copyright (C) 2017  Firespring
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+const QueryBuilder = require('./../aws/queryBuilder');
+const Repository = require('./repository');
+const RepositoryHelper = require('./../helpers/repository');
+const ResourceNotFoundException = require('./../exceptions/resourceNotFound');
+const Sponsor = require('./../models/sponsor');
+const SponsorTier = require('./../models/sponsorTier');
+const SponsorTiersRepository = require('./sponsorTiers');
+
+/**
+ * SponsorRepository constructor
+ *
+ * @constructor
+ */
+function SponsorRepository() {
+	Repository.call(this, RepositoryHelper.SponsorsTable);
+}
+
+/**
+ * Extend the base Repository
+ *
+ * @type {Repository}
+ */
+SponsorRepository.prototype = new Repository();
+
+/**
+ * Get a Sponsor
+ *
+ * @param {String} sponsorTierUuid
+ * @param {String} uuid
+ * @return {Promise}
+ */
+SponsorRepository.prototype.get = function (sponsorTierUuid, uuid) {
+	const repository = this;
+	const sponsorTiersRepository = new SponsorTiersRepository();
+	return new Promise(function (resolve, reject) {
+		sponsorTiersRepository.get(sponsorTierUuid).then(function () {
+			const builder = new QueryBuilder('scan');
+			builder.condition('uuid', '=', uuid).filter('sponsorTierUuid', '=', sponsorTierUuid);
+			repository.query(builder).then(function (data) {
+				if (data.Items.length === 1) {
+					resolve(new Sponsor(data.Items[0]));
+				}
+				reject(new ResourceNotFoundException('The specified sponsor does not exist.'));
+			}).catch(function (err) {
+				reject(err);
+			});
+		}).catch(function (err) {
+			reject(err);
+		});
+	});
+};
+
+/**
+ * Get all Sponsors for this Sponsor Tier
+ *
+ * @param {String} sponsorTierUuid
+ * @return {Promise}
+ */
+SponsorRepository.prototype.getAll = function (sponsorTierUuid) {
+	const repository = this;
+	const sponsorTiersRepository = new SponsorTiersRepository();
+	return new Promise(function (resolve, reject) {
+		sponsorTiersRepository.get(sponsorTierUuid).then(function () {
+			const builder = new QueryBuilder('scan');
+			builder.index('sponsorTierUuidIndex').condition('sponsorTierUuid', '=', sponsorTierUuid);
+			repository.query(builder).then(function (data) {
+				const results = [];
+				if (data.Items) {
+					data.Items.forEach(function (item) {
+						results.push(new Sponsor(item));
+					});
+				}
+				resolve(results);
+			}).catch(function (err) {
+				reject(err);
+			});
+		}).catch(function (err) {
+			reject(err);
+		});
+	});
+};
+
+/**
+ * Delete a Sponsor
+ *
+ * @param {String} sponsorTierUuid
+ * @param {String} uuid
+ * @return {Promise}
+ */
+SponsorRepository.prototype.delete = function (sponsorTierUuid, uuid) {
+	const repository = this;
+	const sponsorTiersRepository = new SponsorTiersRepository();
+	return new Promise(function (resolve, reject) {
+		sponsorTiersRepository.get(sponsorTierUuid).then(function () {
+			repository.deleteByKey('uuid', uuid).then(function () {
+				resolve();
+			}).catch(function (err) {
+				reject(err);
+			});
+		}).catch(function (err) {
+			reject(err);
+		})
+	});
+};
+
+/**
+ * Create or update a Donation Tier
+ *
+ * @param {String} sponsorTierUuid
+ * @param {Sponsor} model
+ */
+SponsorRepository.prototype.save = function (sponsorTierUuid, model) {
+	const repository = this;
+	const sponsorTiersRepository = new SponsorTiersRepository();
+	return new Promise(function (resolve, reject) {
+		sponsorTiersRepository.get(sponsorTierUuid).then(function () {
+			if (!(model instanceof SponsorTier)) {
+				reject(new Error('invalid SponsorTier model'));
+			}
+			model.validate().then(function () {
+				const key = {
+					uuid: model.uuid
+				};
+				repository.put(key, model.except(['uuid'])).then(function (data) {
+					resolve(new Sponsor(data.Attributes));
+				}).catch(function (err) {
+					reject(err);
+				});
+			}).catch(function (err) {
+				reject(err);
+			});
+		}).catch(function (err) {
+			reject(err);
+		});
+	});
+};
+
+/**
+ * Batch delete Sponsors
+ *
+ * @param {string} sponsorTierUuid
+ * @param {[]} models
+ * @return {Promise}
+ */
+SponsorRepository.prototype.batchRemove = function (sponsorTierUuid, models) {
+	const repository = this;
+	const sponsorTiersRepository = new SponsorTiersRepository();
+	return new Promise(function (resolve, reject) {
+		sponsorTiersRepository.get(sponsorTierUuid).then(function () {
+			return repository.batchDelete(models);
+		}).then(function () {
+			resolve();
+		}).catch(function (err) {
+			reject(err);
+		});
+	});
+};
+
+module.exports = SponsorRepository;
