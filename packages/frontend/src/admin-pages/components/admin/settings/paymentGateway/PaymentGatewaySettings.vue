9246424aab579e1950b5c9a23cf442b0f7df44fa
paul.kubitschek <paul.kubitschek@firespring.com>
2018-02-05 16:18:26 -0600

    [GD-212] [GD-870] Added apiErrors to the paymentSpring gateway Settings .vue

--- packages/frontend/src/admin-pages/components/admin/settings/paymentGateway/PaymentGatewaySettings.vue
+++ packages/frontend/src/admin-pages/components/admin/settings/paymentGateway/PaymentGatewaySettings.vue
@@ -31,7 +31,7 @@
                 </div>
 
                 <div class="o-app-main-content">
-
+                    <api-error v-model="apiError"></api-error>
                     <form v-on:submit="submit">
 
                         <section class="c-page-section c-page-section--border c-page-section--shadow c-page-section--segmented">
@@ -185,7 +185,8 @@
 				},
 
 				// Errors
-				formErrors: {}
+				formErrors: {},
+                apiError: {},
 			};
 		},
 		beforeRouteEnter: function (to, from, next) {
@@ -305,8 +306,8 @@
 						vue.$router.push({name: 'settings-list'});
 					}
 				}).catch(function (err) {
-					vue.clearModals();
-					console.log(err);
+					vue.removeModal('spinner');
+                    vue.apiError = err.response.data.errors;
 				});
 			}
 		}
