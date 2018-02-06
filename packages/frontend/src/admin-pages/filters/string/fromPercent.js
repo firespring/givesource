0edaed5ad4c2ecc6e28d7b1effec05dcd3c55c34
Carmen Knudson <carmen.knudson@firespring.com>
2018-02-06 15:28:46 -0600

    [GD-783] [GD-895] Update admin ui to allow offline transaction fees to be configured.

---
+++ packages/frontend/src/admin-pages/filters/string/fromPercent.js
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2017  Firespring
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * Convert the provided value from a decimal to a whole percent.
+ * For example .029 would be converted to 2.9.
+ *
+ * @param {*} value
+ * @returns {number}
+ */
+function fromPercent(value) {
+	return (value * 100);
+}
+
+export default fromPercent;
