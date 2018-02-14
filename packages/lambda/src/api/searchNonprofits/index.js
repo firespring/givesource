a97434408f815df73a2cc6417ef8f069ff561d0a
Carmen Knudson <carmen.knudson@firespring.com>
2018-02-14 09:05:02 -0600

    [GD-725] [GD-795] Update api to allow the match fund nonprofit to be excluded.

--- packages/lambda/src/api/searchNonprofits/index.js
+++ packages/lambda/src/api/searchNonprofits/index.js
@@ -18,22 +18,37 @@
 const HttpException = require('./../../exceptions/http');
 const NonprofitsRepository = require('./../../repositories/nonprofits');
 const Request = require('./../../aws/request');
+const SettingsRepository = require('./../../repositories/settings');
+const SettingHelper = require('./../../helpers/setting');
 
 exports.handle = function (event, context, callback) {
 	const repository = new NonprofitsRepository();
 	const request = new Request(event, context);
+	const settingsRepository = new SettingsRepository();
+	const includeMatchFund = parseInt(request.queryParam('includeMatchFund', 1));
+	let matchFundNonprofitUuid = null;
 
 	request.validate().then(function () {
+		if (!includeMatchFund) {
+			return settingsRepository.get(SettingHelper.SETTING_MATCH_FUND_NONPROFIT_UUID).then(function (setting) {
+				matchFundNonprofitUuid = setting.value;
+			}).catch(function (err) {
+				console.log(err);
+			});
+		}
+
+		return Promise.resolve();
+	}).then(function () {
 		if (!request.queryParam('category', false) && !request.queryParam('legalName', false) && !request.queryParam('status', false)) {
 			return Promise.reject(new Error('Missing required query parameter: category, legalName or status'));
 		}
 		if (request.queryParam('category', false)) {
-			return repository.search(['category1', 'category2', 'category3'], request.queryParam('category'), transformFilters(request.queryParamsExcept(['category'])));
+			return repository.search(['category1', 'category2', 'category3'], request.queryParam('category'), transformFilters(request.queryParamsExcept(['category']), matchFundNonprofitUuid));
 		}
 		if (request.queryParam('legalName', false)) {
-			return repository.search(['legalNameSearch'], request.queryParam('legalName'), transformFilters(request.queryParamsExcept(['legalName'])));
+			return repository.search(['legalNameSearch'], request.queryParam('legalName'), transformFilters(request.queryParamsExcept(['legalName']), matchFundNonprofitUuid));
 		}
-		return repository.search(['status'], request.queryParam('status'), transformFilters(request.queryParamsExcept(['status'])));
+		return repository.search(['status'], request.queryParam('status'), transformFilters(request.queryParamsExcept(['status']), matchFundNonprofitUuid));
 	}).then(function (response) {
 		const results = response.map(function (model) {
 			return model.all();
@@ -44,10 +59,21 @@ exports.handle = function (event, context, callback) {
 	});
 };
 
-const transformFilters = function (filters) {
+const transformFilters = function (filters, matchFundNonprofitUuid) {
 	if (filters.hasOwnProperty('legalName')) {
 		filters.legalNameSearch = filters.legalName;
 		delete filters.legalName;
+	}
+
+	if (filters.hasOwnProperty('includeMatchFund')) {
+		const includeMatchFund = parseInt(filters.includeMatchFund);
+		if (!includeMatchFund && matchFundNonprofitUuid) {
+			filters.uuid = {
+				conditional: '!=',
+				value: matchFundNonprofitUuid
+			};
+		}
+		delete filters.includeMatchFund;
 	}
 	return filters;
 };
