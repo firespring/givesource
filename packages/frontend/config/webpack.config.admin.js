1e66aac7e24905ba8b7b34f25beef6d0f2adff51
cj.ohara <cj.ohara@firespring.com>
2018-02-23 14:22:50 -0600

    [GD-936] [GD-951] Simplified webpack configs for the frontend, fixed stack creation failures.

--- packages/frontend/config/webpack.config.admin.js
+++ packages/frontend/config/webpack.config.admin.js
@@ -18,7 +18,99 @@
 const dotenv = require('dotenv');
 dotenv.config({path: `${__dirname}/../../../.env`});
 
+const path = require('path');
+const webpack = require('webpack');
+const BrowserSyncSpa = require('browser-sync-middleware-spa');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 module.exports = function () {
 	const env = process.env.NODE_ENV || 'development';
-	return require(`./webpack.config.admin.${env}.js`);
+	const config = {
+		module: {
+			rules: [
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader',
+					exclude: /node_modules/,
+					options: {
+						loaders: {
+							js: 'babel-loader'
+						}
+					}
+				},
+				{
+					test: /\.(jpe?g|png|gif|svg)$/i,
+					loader: 'file-loader?name=assets/img/[name].[ext]'
+				},
+				{
+					test: /\.css$/,
+					loader: 'style-loader!css-loader'
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+				}
+			]
+		},
+		resolve: {
+			alias: {
+				'vue$': 'vue/dist/vue.esm.js'
+			}
+		},
+		target: 'web',
+		entry: [
+			'./src/admin-pages/app.js'
+		],
+		output: {
+			path: path.resolve(__dirname, './../build/admin-pages'),
+			filename: 'bundle.js',
+			publicPath: '/',
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env': {
+					NODE_ENV: `"${process.env.NODE_ENV}"`,
+				}
+			}),
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery',
+				Promise: 'es6-promise-promise',
+				'window.jQuery': 'jquery'
+			}),
+			new CopyWebpackPlugin([
+				{from: './config/**/*.json', to: '[name].[ext]'},
+				{from: './src/admin-pages/assets/css', to: 'assets/css'},
+				{from: './src/admin-pages/assets/img', to: 'assets/img'}
+			], {
+				ignore: [
+					'deploy-info.json'
+				]
+			}),
+			new HtmlWebpackPlugin({
+				template: 'src/admin-pages/index.html'
+			}),
+			new BrowserSyncPlugin({
+				host: 'localhost',
+				port: 3000,
+				server: {
+					baseDir: './../frontend/build/admin-pages',
+					index: 'index.html',
+					middleware: [
+						BrowserSyncSpa(/^[^\.]+$/, __dirname + '/../build/admin-pages/index.html')
+					]
+				},
+				files: ['bundle.js', 'assets/**/*.css', 'settings.json'],
+				open: false
+			}),
+		]
+	};
+	if (env === 'production') {
+		config.plugins.push(new UglifyJsPlugin());
+	}
+	return config;
 };
--- packages/frontend/config/webpack.config.public.js
+++ packages/frontend/config/webpack.config.public.js
@@ -18,7 +18,99 @@
 const dotenv = require('dotenv');
 dotenv.config({path: `${__dirname}/../../../.env`});
 
+const path = require('path');
+const webpack = require('webpack');
+const BrowserSyncSpa = require('browser-sync-middleware-spa');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FetchDynamicContent = require('./../bin/fetch-dynamic-content');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const WebpackOnBuildPlugin = require('on-build-webpack');
+
 module.exports = function () {
 	const env = process.env.NODE_ENV || 'development';
-	return require(`./webpack.config.public.${env}.js`);
+	const config = {
+		module: {
+			rules: [
+				{
+					test: /\.vue$/,
+					loader: 'vue-loader',
+					exclude: /node_modules/,
+					options: {
+						loaders: {
+							js: 'babel-loader'
+						}
+					}
+				},
+				{
+					test: /\.(jpe?g|png|gif|svg)$/i,
+					loader: 'file-loader?name=assets/img/[name].[ext]'
+				},
+				{
+					test: /\.css$/,
+					loader: 'style-loader!css-loader'
+				},
+				{
+					test: /\.js$/,
+					exclude: /node_modules/,
+					loader: 'babel-loader'
+				}
+			]
+		},
+		resolve: {
+			alias: {
+				'vue$': 'vue/dist/vue.esm.js'
+			}
+		},
+		target: 'web',
+		entry: [
+			'./src/public-pages/app.js'
+		],
+		output: {
+			path: path.resolve(__dirname, './../build/public-pages'),
+			filename: 'bundle.js',
+			publicPath: '/',
+		},
+		plugins: [
+			new webpack.DefinePlugin({
+				'process.env': {
+					NODE_ENV: `"${process.env.NODE_ENV}"`,
+				}
+			}),
+			new webpack.ProvidePlugin({
+				$: 'jquery',
+				jQuery: 'jquery',
+				Promise: 'es6-promise-promise',
+				'window.jQuery': 'jquery'
+			}),
+			new CopyWebpackPlugin([
+				{from: './src/public-pages/assets/css', to: 'assets/css'},
+				{from: './src/public-pages/assets/img', to: 'assets/img'},
+				{from: './src/public-pages/assets/temp', to: 'assets/temp'}
+			]),
+			new HtmlWebpackPlugin({
+				template: 'src/public-pages/templates/index.mustache',
+				filename: 'templates/index.mustache'
+			}),
+			new BrowserSyncPlugin({
+				host: 'localhost',
+				port: 3002,
+				server: {
+					baseDir: './../frontend/build/public-pages',
+					index: 'index.html',
+					middleware: [
+						BrowserSyncSpa(/^[^\.]+$/, __dirname + '/../build/public-pages/index.html')
+					]
+				},
+				files: ['bundle.js', 'assets/**/*.css'],
+				open: false
+			}),
+			new WebpackOnBuildPlugin(FetchDynamicContent.fetch),
+		]
+	};
+	if (env === 'production') {
+		config.plugins.push(new UglifyJsPlugin());
+	}
+	return config;
 };
