e061fb963237f39fec05bc131b63f9c376d1192b
Joe Ebmeier <joe.ebmeier@firespring.com>
2020-09-03 15:19:39 -0500

    WIP

--- packages/lambda/src/models/connect.js
+++ packages/lambda/src/models/connect.js
@@ -23,7 +23,7 @@ const Sequelize = require('sequelize');
 module.exports = function() {
 	const readwriteSecretId = process.env.AWS_STACK_NAME + "/ReadwriteUserSecret";
 	const secretsManager = new SecretsManager();
-	secretsManager.getSecretValue(process.env.AWS_REGION, readwriteSecretId).then(function (secret) {
+	return secretsManager.getSecretValue(process.env.AWS_REGION, readwriteSecretId).then(function (secret) {
 		const readwriteSecret = JSON.parse(secret.SecretString);
 		return new Sequelize({
 			host: readwriteSecret.host,
