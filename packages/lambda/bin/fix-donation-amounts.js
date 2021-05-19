95277b43acc9d796da452165ed426cce93b55bf5
Joe Ebmeier <joe.ebmeier@firespring.com>
2021-05-19 16:40:43 +0000

    WIP

--- packages/lambda/bin/fix-donation-amounts.js
+++ packages/lambda/bin/fix-donation-amounts.js
@@ -19,6 +19,7 @@ require('./config/bootstrap').bootstrap();
 const config = require('config');
 const DonationHelper = require('./../src/helpers/donation');
 const DonationsRepository = require('./../src/repositories/donations');
+const PaymentTransactionRepository = require('./../src/repositories/paymentTransactions');
 const SettingHelper = require('./../src/helpers/setting');
 const SettingsRepository = require('./../src/repositories/settings');
 const loadModels = require('./../src/models/index');
@@ -28,37 +29,56 @@ const fixDonations = function() {
     const settingsRepository = new SettingsRepository();
 
     let numChanged = 0;
-    let settings = {};
-    settings[SettingHelper.SETTING_PAYMENT_GATEWAY_TRANSACTION_FEE_FLAT_RATE] = null;
-    settings[SettingHelper.SETTING_PAYMENT_GATEWAY_TRANSACTION_FEE_PERCENTAGE] = null;
+    let transactionFlatFee = 0;
+    let transactionPercentFee = 0;
 
-    settingsRepository.batchGet(Object.keys(settings)).then((response) => {
-        response.forEach((setting) => {
-            settings[setting.key] = setting.value;
-        });
-    }).then(function () {
-        return getDonationsData();
-    }).then(function (donations) {
-        let transactionFlatFee = settings[SettingHelper.SETTING_PAYMENT_GATEWAY_TRANSACTION_FEE_FLAT_RATE];
-        transactionFlatFee = transactionFlatFee ? parseInt(transactionFlatFee) : 0;
+    settingsRepository.get(SettingHelper.SETTING_PAYMENT_GATEWAY_TRANSACTION_FEE_FLAT_RATE).then(function(flatFee) {
+        transactionFlatFee = flatFee.value;
+        return settingsRepository.get(SettingHelper.SETTING_PAYMENT_GATEWAY_TRANSACTION_FEE_PERCENTAGE);
+    }).then(function (feePercentage) {
+        transactionPercentFee = feePercentage.value;
+//        return getDonationsData();
+//    }).then(function (donations) {
+        return queryPaymentTransactions();
+    }).then(function (paymentTransactions) {
+        paymentTransactions.forEach(function (paymentTransaction) {
+            let subtotal = 0;
+            let fees = 0;
+            paymentTransaction.Donations.forEach(function (donation) {
+                donation.fees = DonationHelper.calculateFees(
+                    donation.isOfflineDonation,
+                    donation.isFeeCovered,
+                    donation.subtotal,
+                    transactionFlatFee,
+                    transactionPercentFee
+                );
 
-        let transactionPercentFee = settings[SettingHelper.SETTING_PAYMENT_GATEWAY_TRANSACTION_FEE_PERCENTAGE];
-        transactionPercentFee = transactionPercentFee ? parseFloat(transactionPercentFee) : 0;
+                //
+                donation.total = donation.amountForNonprofit + donation.fees;
 
-        donations.rows.forEach(function (donation) {
-            donation.fees = DonationHelper.calculateFees(
-                donation.isOfflineDonation,
-                donation.isFeeCovered,
-                donation.subtotal,
-                transactionFlatFee,
-                transactionPercentFee
-            );
-            donation.total = donation.amountForNonprofit + donation.fees;
-            //console.log(donation);
-            //console.log(donation.donor);
-            //console.log(donation.paymentTransaction);
-            //donation.paymentTransaction.transactionAmount = donation.total;
+                subtotal += donation.subtotal;
 
+                // TODO if fee is not covered we need to change the amount for nonprofit???
+                if (donation.isFeeCovered) {
+                    fees += donation.fees;
+                }
+
+                //donation.paymentTransaction.transactionAmount = donation.total;
+
+                if (donation.changed())
+                {
+                    numChanged += 1;
+                    showChanges(donation);
+                    // TODO: Update
+                }
+            });
+
+            paymentTransaction.transactionAmount = subtotal + fees;
+            if (paymentTransaction.changed())
+            {
+                showChanges(paymentTransaction);
+            }
+            foo.bar;
             // Only update if the values have changed
             if (donation.changed())
             {
@@ -67,39 +87,70 @@ const fixDonations = function() {
                 // TODO: Update
             }
         });
-        console.log(`CHANGED ${numChanged} rows`);
+        //console.log(`CHANGED ${numChanged} rows`);
 
+
+
+
+
+
+
+
+
+
+
+//    }).then(() => {
+//        const body = {
+//            donations: donations.map((donation) => {
+//                donation.timezone = settings.EVENT_TIMEZONE;
+//                donation.total = donation.formattedAmount;
+//                return donation;
+//            }),
+//            donor: donor,
+//            paymentTransaction: paymentTransaction,
+//        };
+//        // there was a mutate function looking like to fix the timezones
+//        lambda.invoke(process.env.AWS_REGION, process.env.AWS_STACK_NAME + '-SendDonationsReceiptEmail', {body: body});
+//    }).then(() => {
+//        const body = {
+//            donations: donations.map((donation) => {
+//                donation.timezone = settings.EVENT_TIMEZONE;
+//        donation.subtotal = donation.formattedSubtotal;
+//                return donation;
+//            }),
+//      donor: donor
+//        };
+//        lambda.invoke(process.env.AWS_REGION, process.env.AWS_STACK_NAME + '-SendDonationNotificationEmail', {body: body});
+//        callback();
     }).catch(function (err) {
         console.log(`error: ${err}`);
     });
 };
 
 /**
- * Get donation data
+ * Get paymentTransactions data
  *
  * @return {Promise}
  */
-const getDonationsData = function () {
-    const donationsRepository = new DonationsRepository();
-    const settingsRepository = new SettingsRepository();
+const queryPaymentTransactions = function () {
+    const paymentTransactionRepository = new PaymentTransactionRepository();
 
     let allModels;
     return loadModels().then(function (models) {
         allModels = models;
 
+//            include: [
+//                {model: allModels.PaymentTransaction},
+//                {model: allModels.Donations}
+//            ],
         const params = {
-            include: [
-                {model: allModels.PaymentTransaction},
-                {model: allModels.Donor}
-            ],
+            include: [{model: allModels.Donation}],
             where: {
-                paymentTransactionIsTestMode: 0,
-                isFeeCovered: 1,
-                isDeleted: 0,
+                IsTestMode: 0,
                 createdAt: {[Sequelize.Op.lt]: new Date('2021-05-12')}
             }
         };
-        return donationsRepository.queryDonations(params);
+        return paymentTransactionRepository.getAll(params);
     }).catch(function (err) {
         console.log(err);
     });
@@ -108,10 +159,11 @@ const getDonationsData = function () {
     return promise;
 };
 
-const showChanges = function (donation) {
-    let message = `Donation (Id: ${donation.id})`;
-    donation.changed().forEach(function (columnName) {
-        message += ` ${columnName}: ${donation._previousDataValues[columnName]} => ${donation.dataValues[columnName]},`;
+const showChanges = function (thing) {
+    let message = `${thing} (Id: ${thing.id})`;
+    console.log(message);
+    thing.changed().forEach(function (columnName) {
+        message += ` ${columnName}: ${thing._previousDataValues[columnName]} => ${thing.dataValues[columnName]},`;
     });
     console.log(message);
 };
