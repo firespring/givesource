c05de1d06ae0df069a8970104fd3133646bd5128
Joe Ebmeier <joe.ebmeier@firespring.com>
2021-05-27 14:19:27 -0500

    [GD-1819] Add readonly user to default RDS for givesource

--- packages/lambda/src/database/bootstrapDatabase/index.js
+++ packages/lambda/src/database/bootstrapDatabase/index.js
@@ -57,7 +57,8 @@ exports.handle = function (event, context, callback) {
 		return Promise.all([
 			secretsManager.getSecretValue(process.env.AWS_REGION, process.env.ADMIN_DATABASE_SECRET_ID),
 			secretsManager.getSecretValue(process.env.AWS_REGION, process.env.MAINTENANCE_DATABASE_SECRET_ID),
-			secretsManager.getSecretValue(process.env.AWS_REGION, process.env.READWRITE_DATABASE_SECRET_ID)
+			secretsManager.getSecretValue(process.env.AWS_REGION, process.env.READWRITE_DATABASE_SECRET_ID),
+			secretsManager.getSecretValue(process.env.AWS_REGION, process.env.READONLY_DATABASE_SECRET_ID)
 		]);
 	}).then(function (secrets) {
 		const dbHost = process.env.DATABASE_HOST;
@@ -65,6 +66,7 @@ exports.handle = function (event, context, callback) {
 		const adminSecret = JSON.parse(secrets.find(it => it['Name'] === process.env.ADMIN_DATABASE_SECRET_ID).SecretString);
 		const maintenanceSecret = JSON.parse(secrets.find(it => it['Name'] === process.env.MAINTENANCE_DATABASE_SECRET_ID).SecretString);
 		const readwriteSecret = JSON.parse(secrets.find(it => it['Name'] === process.env.READWRITE_DATABASE_SECRET_ID).SecretString);
+		const readonlySecret = JSON.parse(secrets.find(it => it['Name'] === process.env.READONLY_DATABASE_SECRET_ID).SecretString);
 
 		sequelize = new Sequelize({
 			host: adminSecret.host,
@@ -85,7 +87,9 @@ exports.handle = function (event, context, callback) {
 			'CREATE USER IF NOT EXISTS "' + maintenanceSecret.username + '"@"%" IDENTIFIED BY "' + maintenanceSecret.password + '"; ' +
 			'GRANT ALL PRIVILEGES ON `' + dbName+ '`.* TO "' + maintenanceSecret.username + '"@"%"; ' +
 			'CREATE USER IF NOT EXISTS "' + readwriteSecret.username + '"@"%" IDENTIFIED BY "' + readwriteSecret.password + '"; ' +
-			'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES, EXECUTE ON `' + dbName+ '`.* TO "' + readwriteSecret.username + '"@"%";'
+			'GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES, EXECUTE ON `' + dbName+ '`.* TO "' + readwriteSecret.username + '"@"%";' +
+			'CREATE USER IF NOT EXISTS "' + readonlySecret.username + '"@"%" IDENTIFIED BY "' + readonlySecret.password + '"; ' +
+			'GRANT SELECT ON `' + dbName+ '`.* TO "' + readonlySecret.username + '"@"%";'
 		);
 	}).then(function () {
 		response.send(event, context, response.SUCCESS);
