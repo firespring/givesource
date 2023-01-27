1dc32cf20d898cac3da685d55fbb8dadf251debb
Mike Keiser <mike.keiser@firespring.com>
2023-01-27 10:44:02 -0600

    [GD-1920] wip

--- packages/frontend/bin/fetch-dynamic-content.js
+++ packages/frontend/bin/fetch-dynamic-content.js
@@ -26,11 +26,17 @@ const S3 = require('./aws/s3');
 
 exports.fetch = () => {
 	const s3 = new S3();
+	const configDir = path.resolve(__dirname, './../build/public-pages/assets/css');
 	s3.getObject(config.get('stack.AWS_REGION'), deployInfo.PublicPagesS3BucketName, 'assets/css/custom.css').then(response => {
-		const configDir = path.resolve(__dirname, './../build/public-pages/assets/css');
 		fs.writeFileSync(configDir + '/custom.css', response.Body);
 		console.log('custom.css downloaded from S3');
 	}).catch(function (err) {
-		console.log(err);
+		console.log('No custom css found. Using empty file', {
+			region: config.get('stack.AWS_REGION'),
+			bucket: deployInfo.PublicPagesS3BucketName,
+			path: 'assets/css/custom.css'
+		});
+		// console.log(err); // if you are expecting a custom.css err may have more info on the issue
+		fs.writeFileSync(configDir + '/custom.css', '/* No custom styles */');
 	});
 };
