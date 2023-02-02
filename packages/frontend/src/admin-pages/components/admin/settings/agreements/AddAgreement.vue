af2f9f561cade74b1cabcb3366bd8a5e125a7c10
Mike Keiser <mike.keiser@firespring.com>
2023-02-02 05:07:48 -0600

    [GD-1916] wip

--- packages/frontend/src/admin-pages/components/admin/settings/agreements/AddAgreement.vue
+++ packages/frontend/src/admin-pages/components/admin/settings/agreements/AddAgreement.vue
@@ -16,78 +16,95 @@
 
 <template>
   <div class="o-app">
-    <navigation></navigation>
+    <navigation />
     <main class="o-app__main o-app__main--compact">
       <div class="o-app_main-content o-app_main-content--md">
-
         <div class="o-page-header">
           <div class="o-page-header__text">
             <nav class="o-page-header-nav c-breadcrumb">
               <span><router-link :to="{ name: 'settings-list' }">Settings</router-link></span>
               <span><router-link :to="{ name: 'settings-agreements' }">Event Agreements</router-link></span>
             </nav>
-            <h1 class="o-page-header-title">Add Agreement</h1>
+            <h1 class="o-page-header-title">
+              Add Agreement
+            </h1>
           </div>
         </div>
 
         <div class="o-app-main-content">
-          <api-error v-model="apiError"></api-error>
-
-          <!--    begin main content      -->
-
-
+          <api-error v-model="apiError" />
 
           <!-- BEGIN form -->
-          <form v-on:submit="submit">
-
+          <form @submit="submit">
             <section class="c-page-section c-page-section--border c-page-section--shadow c-page-section--segmented">
-
               <header class="c-page-section__header">
                 <div class="c-page-section-header-text">
-
-                  <h2 class="c-page-section-title">Agreement Info</h2>
-
+                  <h2 class="c-page-section-title">
+                    Agreement Info
+                  </h2>
                 </div>
               </header>
 
               <div class="c-page-section__main">
-
                 <div class="c-form-item c-form-item--text c-form-item--required">
-
                   <div class="c-form-item__label">
-                    <label for="agreementTitle" class="c-form-item-label-text">Agreement Title</label>
+                    <label
+                      for="agreementTitle"
+                      class="c-form-item-label-text"
+                    >Agreement Title</label>
                   </div>
 
                   <div class="c-form-item__control">
-
-                    <input v-model="formData.agreementTitle" type="text" name="agreementTitle" id="agreementTitle" maxlength="200"
-                           :class="{ 'has-error': formErrors.agreementTitle }" required v-auto-focus>
-                    <div v-if="formErrors.EVENT_TITLE" class="c-notes c-notes--below c-notes--bad c-form-control-error">
+                    <input
+                      id="agreementTitle"
+                      v-model="formData.agreementTitle"
+                      v-auto-focus
+                      type="text"
+                      name="agreementTitle"
+                      maxlength="200"
+                      :class="{ 'has-error': formErrors.agreementTitle }"
+                      required
+                    >
+                    <div
+                      v-if="formErrors.agreementTitle"
+                      class="c-notes c-notes--below c-notes--bad c-form-control-error"
+                    >
                       {{ formErrors.agreementTitle }}
                     </div>
-
                   </div>
-
-
                 </div>
 
                 <div class="c-form-item c-form-item--textarea c-form-item--required">
-
                   <div class="c-form-item__label">
-                    <label for="agreementText" class="c-form-item-label-text">Agreement Text</label>
+                    <label
+                      for="agreementText"
+                      class="c-form-item-label-text"
+                    >Agreement Text</label>
                   </div>
 
                   <div class="c-form-item__control">
-                    <forms-ckeditor v-model="formData.agreementText" :loaded="loaded" id="formText" type="advanced"></forms-ckeditor>
-<!--                    todo form errors?-->
+                    <forms-ckeditor
+                      id="formText"
+                      v-model="formData.agreementText"
+                      type="advanced"
+                    />
+                    <div
+                        v-if="formErrors.agreementText"
+                        class="c-notes c-notes--below c-notes--bad c-form-control-error"
+                    >
+                      {{ formErrors.agreementText }}
+                    </div>
                   </div>
-
                 </div>
 
                 <div class="c-form-item c-form-item--radio">
-
                   <div class="c-form-item__label">
-                    <div class="c-form-item-label-text" id="isRequired">Is This Agreement Required?</div>
+                    <div
+                      id="isRequired"
+                      class="c-form-item-label-text"
+                    >
+                      Is This Agreement Required?
+                    </div>
 
                     <div class="c-notes">
                       If set to "Yes," then the registrant must agree to this agreement in order to complete their registration.
@@ -95,43 +112,54 @@
                   </div>
 
                   <div class="c-form-item__control">
-
-                    <ul class="c-input-list c-input-list--radio" aria-labelledby="isRequired">
+                    <ul
+                      class="c-input-list c-input-list--radio"
+                      aria-labelledby="isRequired"
+                    >
                       <li>
-                        <input v-model="formData.isRequired" type="radio" name="isRequired" id="isRequired-1" value="1">
+                        <input
+                          id="isRequired-1"
+                          v-model="formData.isRequired"
+                          type="radio"
+                          name="isRequired"
+                          value="1"
+                        >
                         <label for="isRequired-1">Yes</label>
                       </li>
                       <li>
-                        <input v-model="formData.isRequired" type="radio" name="isRequired" id="isRequired-0" value="0">
+                        <input
+                          id="isRequired-0"
+                          v-model="formData.isRequired"
+                          type="radio"
+                          name="isRequired"
+                          value="0"
+                        >
                         <label for="isRequired-0">No</label>
                       </li>
                     </ul>
-
                   </div>
-
                 </div>
               </div>
-              <pre>
-                {{formData}}
-              </pre>
-
             </section>
 
             <!-- BEGIN form actions -->
             <footer class="c-form-actions">
-
-              <button type="submit" class="c-btn">Save Changes</button>
-              <router-link :to="{ name: 'settings-agreements' }" class="c-btn c-btn--neutral c-btn--text">Cancel</router-link>
-
+              <button
+                type="submit"
+                class="c-btn"
+              >
+                Save Changes
+              </button>
+              <router-link
+                :to="{ name: 'settings-agreements' }"
+                class="c-btn c-btn--neutral c-btn--text"
+              >
+                Cancel
+              </router-link>
             </footer>
             <!-- END form actions -->
-
           </form>
           <!-- END form -->
-
-
-
-          <!--    end main-content      -->
         </div>
       </div>
     </main>
@@ -139,9 +167,12 @@
 </template>
 
 <script>
-import ComponentCKEditor from './../../../forms/Ckeditor.vue';
+import ComponentCKEditor from './../../../forms/Ckeditor.vue'
 
 export default {
+  components: {
+    'forms-ckeditor': ComponentCKEditor
+  },
   data: function () {
     return {
       settings: [],
@@ -155,91 +186,62 @@ export default {
 
       // Errors
       formErrors: {},
-      apiError: {},
-
-      loaded: true // todo??
-    };
+      apiError: {}
+    }
   },
   watch: {
     formData: {
       handler: function () {
-        const vue = this;
+        const vue = this
         if (Object.keys(vue.formErrors).length) {
-          vue.formErrors = vue.validate(vue.formData, vue.getConstraints());
+          vue.formErrors = vue.validate(vue.formData, vue.getConstraints())
         }
       },
       deep: true
-    },
+    }
   },
   methods: {
     getConstraints: function () {
-      // todo
       return {
-        // DATE_DONATIONS_END: {
-        //   label: 'Donations end date',
-        // },
-        // DATE_DONATIONS_START: {
-        //   label: 'Donations start date',
-        // },
-        // DATE_EVENT_END: {
-        //   label: 'Event end date',
-        //   presence: true,
-        // },
-        // DATE_EVENT_START: {
-        //   label: 'Event start date',
-        //   presence: true,
-        // },
-        // DATE_REGISTRATIONS_END: {
-        //   label: 'Nonprofit registrations end date',
-        // },
-        // DATE_REGISTRATIONS_START: {
-        //   label: 'Nonprofit registrations start date',
-        // },
-        // EVENT_TITLE: {
-        //   label: 'Event title',
-        //   presence: true,
-        // },
-        // EVENT_TIMEZONE: {
-        //   label: 'Event timezone',
-        //   presence: true,
-        // }
-      };
+        agreementTitle: {
+          label: 'Agreement Title',
+          presence: true,
+        },
+        agreementText: {
+          label: 'Agreement Text',
+          presence: true,
+        }
+      }
     },
     submit: function (event) {
-      event.preventDefault();
-      const vue = this;
+      event.preventDefault()
+      const vue = this
 
-      vue.addModal('spinner');
+      vue.addModal('spinner')
 
-      vue.formErrors = vue.validate(vue.formData, vue.getConstraints());
+      vue.formErrors = vue.validate(vue.formData, vue.getConstraints())
       if (Object.keys(vue.formErrors).length) {
-        vue.removeModal('spinner');
+        vue.removeModal('spinner')
       } else {
-        vue.addAgreement();
+        vue.addAgreement()
       }
     },
     addAgreement: function () {
-      const vue = this;
+      const vue = this
 
-      console.log('updateSettings vue.formData', vue.formData);
-
-      const formData = {...vue.formData};
+      const formData = { ...vue.formData }
       vue.$request.post('agreements', formData).then(function (response) {
-        console.log('post response', response)
-        vue.removeModal('spinner');
+        vue.removeModal('spinner')
         if (response.data && response.data.errorMessage) {
-          console.log(response.data);
+          console.log(response.data)
         } else {
-          vue.$router.push({name: 'settings-agreements'});
+          vue.$router.push({ name: 'settings-agreements' })
         }
       }).catch(function (err) {
-        vue.removeModal('spinner');
-        vue.apiError = err.response.data.errors;
-      });
+        vue.removeModal('spinner')
+        vue.apiError = err.response.data.errors
+      })
     }
-  },
-  components: {
-    'forms-ckeditor': ComponentCKEditor,
   }
-};
+}
 </script>
--- packages/frontend/src/admin-pages/components/admin/settings/agreements/AgreementsSettings.vue
+++ packages/frontend/src/admin-pages/components/admin/settings/agreements/AgreementsSettings.vue
@@ -16,66 +16,74 @@
 
 <template>
   <div class="o-app">
-    <navigation></navigation>
+    <navigation />
     <main class="o-app__main o-app__main--compact">
       <div class="o-app_main-content o-app_main-content--md">
-
         <div class="o-page-header">
           <div class="o-page-header__text">
             <nav class="o-page-header-nav c-breadcrumb">
               <span><router-link :to="{ name: 'settings-list' }">Settings</router-link></span>
             </nav>
-            <h1 class="o-page-header-title">Event Agreements</h1>
+            <h1 class="o-page-header-title">
+              Event Agreements
+            </h1>
           </div>
         </div>
 
         <div class="o-app-main-content">
-          <api-error v-model="apiError"></api-error>
-
-<!--    begin main content      -->
-
+          <api-error v-model="apiError" />
 
           <!-- BEGIN header actions -->
           <div class="c-header-actions">
-
             <div>
-              <router-link :to="{ name: 'settings-add-agreement' }" class="c-btn c-btn--sm c-btn--icon" role="button">
-                <i class="fa fa-plus-circle" aria-hidden="true"></i>Add Agreement
+              <router-link
+                :to="{ name: 'settings-add-agreement' }"
+                class="c-btn c-btn--sm c-btn--icon"
+                role="button"
+              >
+                <i
+                  class="fa fa-plus-circle"
+                  aria-hidden="true"
+                />Add Agreement
               </router-link>
             </div>
-
           </div>
           <!-- END header actions -->
 
-          <div><!-- todo? was form tag -->
-
+          <div>
             <!-- BEGIN table -->
             <table class="table-middle table-reorder js-table-reorder">
               <thead>
-              <tr>
-                <th class="u-width-100p">Title</th>
-                <th></th>
-              </tr>
+                <tr>
+                  <th class="u-width-100p">
+                    Title
+                  </th>
+                  <th />
+                </tr>
               </thead>
 
               <tbody class="ui-sortable">
-              <tr v-for="agreement in agreements">
-                <td>
-                  <strong>{{ agreement.agreementTitle }}</strong>
-                </td>
-                <td class="icon">
-                  <a href="#" @click="confirmDeleteAgreement(agreement)" role="button" class="c-btn c-btn--sm c-btn--icon c-btn--bad c-btn--flat js-modal-trigger" rel="modal-confirm-delete"
-                  ><i class="fa fa-trash" aria-hidden="true"></i>Delete</a>
-                </td>
-              </tr>
+                <tr v-for="agreement in agreements">
+                  <td>
+                    <strong>{{ agreement.agreementTitle }}</strong>
+                  </td>
+                  <td class="icon">
+                    <a
+                      href="#"
+                      role="button"
+                      class="c-btn c-btn--sm c-btn--icon c-btn--bad c-btn--flat js-modal-trigger"
+                      rel="modal-confirm-delete"
+                      @click="confirmDeleteAgreement(agreement)"
+                    ><i
+                      class="fa fa-trash"
+                      aria-hidden="true"
+                    />Delete</a>
+                  </td>
+                </tr>
               </tbody>
             </table>
             <!-- END table -->
-
           </div>
-
-
-          <!--    end main-content      -->
         </div>
       </div>
     </main>
@@ -83,60 +91,48 @@
 </template>
 
 <script>
-// import * as Utils from './../../../../helpers/utils';
-// import ComponentDatetime from './../../../forms/Datetime.vue';
-// import ComponentSelectTimeZone from './../../../forms/SelectTimeZone.vue';
-
 export default {
+  beforeRouteEnter: function (to, from, next) {
+    next(function (vue) {
+      vue.$request.get('agreements').then(function (response) {
+        vue.agreements = response.data || []
+      })
+    })
+  },
   data: function () {
     return {
       agreements: [],
       agreementToDelete: null,
-      apiError: {},
-    };
+      apiError: {}
+    }
   },
-  computed: {
-  },
-  beforeRouteEnter: function (to, from, next) {
-    next(function (vue) {
-      vue.$request.get('agreements').then(function (response) {
-        console.log('get agreements', response)
-        vue.agreements = response.data || [];
-      });
-    });
+  created: function () {
+    const vue = this
+
+    vue.bus.$on('deleteAgreement', function () {
+      vue.addModal('spinner')
+      vue.$request.delete('agreements/' + vue.agreementToDelete.id).then(function (response) {
+        vue.agreements = vue.agreements.filter(function (agreement) {
+          return agreement.id !== vue.agreementToDelete.id
+        })
+        vue.removeModal('spinner')
+      }).catch(function (err) {
+        vue.removeModal('spinner')
+        vue.apiError = err.response.data.errors
+      })
+    })
   },
   methods: {
-    confirmDeleteAgreement: function(agreement) {
-      const vue = this;
-      vue.agreementToDelete = agreement;
+    confirmDeleteAgreement: function (agreement) {
+      const vue = this
+      vue.agreementToDelete = agreement
 
       vue.addModal('confirm-delete', {
         modalTitle: 'Do you want to delete the selected item(s)?',
         modalText: 'Proactively harness backward-compatible best practices through enterprise technology. Objectively maximize leveraged catalysts for change with goal-oriented core competencies. Progressively exploit superior technology with end-to-end communities.',
-        callback: 'deleteAgreement',
-      });
-    },
-  },
-  created: function () {
-    const vue = this;
-
-    vue.bus.$on('deleteAgreement', function () {
-      vue.addModal('spinner');
-      vue.$request.delete('agreements/'+vue.agreementToDelete.id).then(function(response){
-        console.log('agreement deleted', { agreement: vue.agreementToDelete, response: response });
-        vue.agreements = vue.agreements.filter(function (agreement) {
-          return agreement.id !== vue.agreementToDelete.id;
-        })
-        vue.removeModal('spinner');
-      }).catch(function (err) {
-        vue.removeModal('spinner');
-        vue.apiError = err.response.data.errors;
-      });
-    });
-  },
-  // components: {
-  //   'forms-datetime': ComponentDatetime,
-  //   'forms-select-time-zone': ComponentSelectTimeZone,
-  // }
-};
+        callback: 'deleteAgreement'
+      })
+    }
+  }
+}
 </script>
