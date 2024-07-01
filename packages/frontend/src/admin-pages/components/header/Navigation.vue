911921adf038859dc492f1e2dac875997da53a08
Jason Bush <jason.bush@firespring.com>
2024-07-01 08:14:59 -0500

    [GD-2103] Remove `window.` from jQuery calls

--- packages/frontend/src/admin-pages/components/header/Navigation.vue
+++ packages/frontend/src/admin-pages/components/header/Navigation.vue
@@ -158,19 +158,19 @@ export default {
       event.preventDefault()
       const vue = this
       if (vue.displayingMenu) {
-        window.$(vue.$refs.oMenubarPopupParent).removeClass('o-menubar-popup-parent--active')
-        window.$(vue.$refs.oMenubarPopup).fadeOut(200)
+        $(vue.$refs.oMenubarPopupParent).removeClass('o-menubar-popup-parent--active')
+        $(vue.$refs.oMenubarPopup).fadeOut(200)
       } else {
-        window.$(vue.$refs.oMenubarPopupParent).addClass('o-menubar-popup-parent--active')
-        window.$(vue.$refs.oMenubarPopup).fadeIn(200)
+        $(vue.$refs.oMenubarPopupParent).addClass('o-menubar-popup-parent--active')
+        $(vue.$refs.oMenubarPopup).fadeIn(200)
       }
       vue.displayingMenu = !vue.displayingMenu
     },
     closeMenu: function () {
       const vue = this
       vue.timer = setTimeout(function () {
-        window.$(vue.$refs.oMenubarPopupParent).removeClass('o-menubar-popup-parent--active')
-        window.$(vue.$refs.oMenubarPopup).fadeOut(200)
+        $(vue.$refs.oMenubarPopupParent).removeClass('o-menubar-popup-parent--active')
+        $(vue.$refs.oMenubarPopup).fadeOut(200)
         vue.displayingMenu = false
       }, 500)
     },
